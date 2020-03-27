b93c1f0d2cf1266a1dd98725f3d0d0d2d5e52b9d
Andrei Sologon <as5519@ic.ac.uk>
2020-03-28 01:59:05 +0200

    Album title added

--- screens/ChapterScreen.js
+++ screens/ChapterScreen.js
@@ -2,6 +2,7 @@ import * as React from 'react';
 import * as Font from 'expo-font';
 import {
   Image,
+  Button,
   ImageBackground,
   Animated,
   Easing,
@@ -16,89 +17,103 @@ import {
 
 import vinylImage from '../assets/images/vinyl.png';
 
-import {containerStyle} from '../styles/Containers.js'
+import {containerStyle} from '../styles/Containers.js';
+
+const screenWidth = Dimensions.get('window').width;
+const screenHeight = Dimensions.get('window').height;
 
 export default class ChapterScreen extends React.Component {
-  constructor() {
-    super();
-    this.albumThumbRotHolder = new Animated.Value(0);
-  }
+
   componentDidMount() {
     this.startAlbumThumbRot();
   }
+  state = {
+    albumThumbRotHolder: new Animated.Value(0),
+    albumAnimationStop: false,
+    albumTitle: "Title"
+  }
 
   startAlbumThumbRot() {
-    this.albumThumbRotHolder.setValue(0);
-    Animated.timing(this.albumThumbRotHolder, {
-      toValue: 1,
+    this.state.albumThumbRotHolder.setValue(0);
+    Animated.timing(this.state.albumThumbRotHolder, {
+      toValue: 360,
       duration: 2000,
       easing: Easing.linear
-    }).start(() => this.startAlbumThumbRot());
+    }).start(() => {
+      if (!this.state.albumAnimationStop)
+        this.startAlbumThumbRot()
+    });
   }
 
   render() {
-    const albumThumbnailRotation = this.albumThumbRotHolder.interpolate({
+    const albumThumbnailRotation = this.state.albumThumbRotHolder.interpolate({
       inputRange: [
-        0, 1
+        0, 360
       ],
       outputRange: ['0deg', '360deg']
     });
-    const screenWidth = Dimensions.get('window').width;
-    const screenHeight = Dimensions.get('window').height;
 
     return (<View style={styles.mainContainer}>
       <View style={containerStyle(100, 25)}></View>
       <View style={[
-          containerStyle(100, 50), {
-            backgroundColor: 'cyan',
+          containerStyle(100, 70), {
             justifyContent: 'flex-start'
           }
         ]}>
-        <ScrollView horizontal={true} pagingEnabled={true}>
-          <View style={{
-              flex: 1,
-              width: screenWidth,
-              justifyContent: 'center',
-              alignItems: 'center'
-            }}>
-            <Animated.Image style={[
-                albumThumbnail(50, 50), {
-                  transform: [
-                    {
-                      rotate: albumThumbnailRotation
-                    }
-                  ]
-                }
-              ]} resizeMode="contain" source={vinylImage}/>
-          </View>
-          <View style={{
-              width: screenWidth,
-              justifyContent: 'center',
-              alignItems: 'center'
-            }}>
-            <Animated.Image style={[
-                albumThumbnail(50, 50), {
-                  transform: [
-                    {
-                      rotate: albumThumbnailRotation
-                    }
-                  ]
-                }
-              ]} resizeMode="contain" source={vinylImage}/>
-          </View>
-        </ScrollView>
+        <View style={containerStyle(100, 60)}>
+          <ScrollView horizontal={true} pagingEnabled={true} showsHorizontalScrollIndicator={false}>
+            <AlbumThumbnail title='bla' thumbnail={vinylImage} rotation={albumThumbnailRotation}/>
+            <AlbumThumbnail title='bla' thumbnail={vinylImage} rotation={albumThumbnailRotation}/>
+            <AlbumThumbnail title='bla' thumbnail={vinylImage} rotation={albumThumbnailRotation}/>
+            <AlbumThumbnail title='bla' thumbnail={vinylImage} rotation={albumThumbnailRotation}/>
+          </ScrollView>
+        </View>
+        <View style={containerStyle(100, 40)}>
+          <Text style={{
+              width: '80%',
+              height: '100%',
+              padding: 20,
+              margin: 20,
+              textAlignVertical: "center",
+              textAlign: "center",
+              fontSize: screenWidth*0.2,
+              fontFamily: 'ArcadeClassic'
+            }}>{this.state.albumTitle}</Text>
+        </View>
       </View>
-      <View style={containerStyle(100, 25)}></View>
+      <View style={containerStyle(100, 15)}></View>
     </View>)
   }
 }
-const albumThumbnail = function(width, height) {
-  return {height: height.toString().concat("%"), width: width.toString().concat("%"), alignSelf: 'center'}
+
+function AlbumThumbnail({title, thumbnail, rotation}) {
+  return (<View style={{
+      width: screenWidth,
+      justifyContent: 'center',
+      alignItems: 'center'
+    }}>
+    <Animated.Image style={[
+        styles.vinylThumbnail, {
+          transform: [
+            {
+              rotate: rotation
+            }
+          ]
+        }
+      ]} resizeMode="contain" source={thumbnail}/>
+  </View>);
 }
 
 const styles = StyleSheet.create({
   mainContainer: {
     ...containerStyle(100, 100),
     backgroundColor: '#40dbe3'
+  },
+  vinylThumbnail: {
+    padding: 20,
+    margin: 20,
+    height: '80%',
+    width: '80%',
+    alignSelf: 'center'
   }
 });
